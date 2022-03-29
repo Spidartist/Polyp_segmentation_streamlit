7fb6f74b98c1c254e2dca21ab64328fabdd6a88e
Spidartist <72654978+Spidartist@users.noreply.github.com>
2022-03-29 22:20:27 +0700

    Update sun_streamlit.py

--- sun_streamlit.py
+++ sun_streamlit.py
@@ -1,12 +1,9 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
-from PIL import Image
 import cv2
 import keras
 from keras.utils.generic_utils import CustomObjectScope
 import tensorflow as tf
-from numpy import unravel_index
 
 
 st.set_page_config(
@@ -45,27 +42,23 @@ st.header("Input the image")
 uploaded_file = st.file_uploader("Choose a image file")
 
 if uploaded_file is not None:
+    
     # Convert the file to an opencv image.
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-    # print(type(file_bytes))
+
     opencv_image = cv2.imdecode(file_bytes, 1)
-    # print(type(opencv_image))
+
     opencv_image = cv2.resize(opencv_image, (256, 256))
     opencv_image = opencv_image / 255.0
-    # print(type(opencv_image))
-    print(opencv_image.shape)
-    # print(unravel_index(opencv_image.argmax(), opencv_image.shape))
 
-    # Now do something with the image! For example, let's display it:
+    print(opencv_image.shape)
+
     st.image(opencv_image, channels="BGR")
     with CustomObjectScope({'iou': iou}):
         model = keras.models.load_model("model.h5")
 
-    # model.evaluate(file_bytes, steps=1)
 
     y_pred = model.predict(np.expand_dims(opencv_image, axis=0))[0] > 0.5
-    # print(y_pred)
-    # print(opencv_image)
 
     h, w, _ = opencv_image.shape
     white_line = np.ones((h, 10, 3)) * 255.0
