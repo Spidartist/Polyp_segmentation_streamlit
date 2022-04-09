af2ebfac5f95a1c0d1c2b482e53fde406603651a
Spidartist <72654978+Spidartist@users.noreply.github.com>
2022-04-09 16:28:48 +0700

    Update sun_streamlit.py

--- sun_streamlit.py
+++ sun_streamlit.py
@@ -71,9 +71,9 @@ if uploaded_file is not None:
 
     st.subheader("Result")
     image = np.concatenate(all_images, axis=1)
-    show_image = np.concatenate(all_images*255.0, axis=1)
+#     show_image = np.concatenate(all_images*255.0, axis=1)
     st.image(image, clamp=True, channels="BGR")
-    success, encoded_image = cv2.imencode('.png', show_image)
+    success, encoded_image = cv2.imencode('.png', mark_parse(y_pred))
     content = encoded_image.tobytes()
     btn = st.download_button(
              label="Download mask",
